8240d4a6a8e9af91a2de015d6dc36900dc895730
Casel Chen <casel.chen@vpon.com>
2016-03-15 10:29:20 +0800

    add headOffset in s3 file name

--- bin/startup.sh
+++ bin/startup.sh
@@ -1,14 +1,18 @@
 #!/usr/bin/env bash
-if [ $# -eq 0 ]
+if [ $# -ne 3 ]
   then
-    echo "Usage: startup.sh <ENV>, ENV options: local, dev, qa"
+    echo "Usage: startup.sh <ENV> <AKKA_PORT> <HTTP_PORT>, ENV options: local, dev, qa"
     exit
 fi
 ENV=$1
+AKKA_PORT=$2
+HTTP_PORT=$3
 cd `dirname $0`/../ansible
 #rm target/${ENV}/*
 echo "generating configuration files ......"
 python genconf.py ${ENV}
 cd ..
 echo "launching application ......"
-sbt -Dlogback.configurationFile=./ansible/target/${ENV}/logback.xml "runMain com.vpon.ssp.report.archive.Main -c ./ansible/target/${ENV}/application.conf"
+#sbt -Dlogback.configurationFile=./ansible/target/${ENV}/logback.xml "runMain com.vpon.ssp.report.dedup.Main -c ./ansible/target/${ENV}/application.conf"
+
+sbt -DPORT=${AKKA_PORT} -Dlogback.configurationFile=./ansible/target/dev/logback.xml "runMain com.vpon.ssp.report.dedup.Main -c ./ansible/target/dev/application.conf -h ${HTTP_PORT}"
